94553835a4f9ddc32fe1d3daa1eadb87ba877771
twenk11k <sdemiral@innova.com.tr>
2021-09-12 16:37:05 +0300

    Add NetworkModule to initialize OkHttp client and retrofit

---
+++ app/src/main/java/com/twenk11k/pokeapibasic/di/NetworkModule.kt
@@ -0,0 +1,29 @@
+package com.twenk11k.pokeapibasic.di
+
+import com.twenk11k.pokeapibasic.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder().build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .build()
+
+}
