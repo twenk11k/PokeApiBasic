0b09882fe5f9133ec9ca6d1c76cb08543199c100
twenk11k <selim_demiral@outlook.com>
2021-09-13 19:28:27 +0300

    Add getImageUrl method to PokemonResponse.Pokemon model

--- app/src/main/java/com/twenk11k/pokeapibasic/model/PokemonResponse.kt
+++ app/src/main/java/com/twenk11k/pokeapibasic/model/PokemonResponse.kt
@@ -6,5 +6,11 @@ data class PokemonResponse(
     data class Pokemon(
         val name: String,
         val url: String
-    )
+    ) {
+        fun getImageUrl(): String {
+            val index = url.split("/".toRegex()).dropLast(1).last()
+            return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$index.png"
+        }
+    }
+
 }
