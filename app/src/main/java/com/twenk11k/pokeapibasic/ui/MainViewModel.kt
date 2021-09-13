4e4c3187f4592242ff357cdf780267553393b138
twenk11k <selim_demiral@outlook.com>
2021-09-13 19:35:19 +0300

    Handle api result from repository in MainViewModel

--- app/src/main/java/com/twenk11k/pokeapibasic/ui/MainViewModel.kt
+++ app/src/main/java/com/twenk11k/pokeapibasic/ui/MainViewModel.kt
@@ -1,9 +1,32 @@
 package com.twenk11k.pokeapibasic.ui
 
+import androidx.databinding.ObservableBoolean
 import androidx.lifecycle.*
+import com.twenk11k.pokeapibasic.model.PokemonInfo
 import com.twenk11k.pokeapibasic.repository.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val mainRepository: MainRepository) : ViewModel()
+class MainViewModel @Inject constructor(private val mainRepository: MainRepository) : ViewModel() {
+
+    private var pokemonInfoLiveData: LiveData<PokemonInfo?>
+
+    private val isLoading = ObservableBoolean(false)
+
+    private val loadTrigger = MutableLiveData(Unit)
+
+    init {
+        pokemonInfoLiveData = loadTrigger.switchMap {
+            mainRepository.getPokemonInfo(
+                onStart = { isLoading.set(true) },
+                onComplete = { isLoading.set(false) },
+                onError = { }
+            ).asLiveData(viewModelScope.coroutineContext + Dispatchers.IO)
+        }
+    }
+
+    fun getPokemonInfoLiveData() = pokemonInfoLiveData
+
+}
