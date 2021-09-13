74c68686c859080560ecb3ad68e7cee2400bdea2
twenk11k <selim_demiral@outlook.com>
2021-09-13 19:32:09 +0300

    Add getPokemonInfo method to MainRepository to get a random pokemon name and its info from pokeapi

--- app/src/main/java/com/twenk11k/pokeapibasic/repository/MainRepository.kt
+++ app/src/main/java/com/twenk11k/pokeapibasic/repository/MainRepository.kt
@@ -1,8 +1,34 @@
 package com.twenk11k.pokeapibasic.repository
 
+import androidx.annotation.WorkerThread
 import com.twenk11k.pokeapibasic.network.PokeService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
+import kotlin.random.Random
 
 class MainRepository @Inject constructor(
     private val pokeService: PokeService
-)
+) {
+
+    @WorkerThread
+    fun getPokemonInfo(
+        onStart: () -> Unit,
+        onComplete: () -> Unit, onError: (String?) -> Unit
+    ) = flow {
+        val pokemonResponse = pokeService.fetchPokemonResponse()
+        if (pokemonResponse.isSuccessful) {
+            pokemonResponse.body()?.results?.let {
+                val randomPokemon = it[Random.nextInt(it.size)]
+                val pokemonInfo = pokeService.fetchPokemonInfo(randomPokemon.name).body()
+                emit(pokemonInfo?.apply { imageUrl = randomPokemon.getImageUrl() })
+            }
+        } else {
+            onError(pokemonResponse.message())
+        }
+    }.onStart { onStart() }.onCompletion { onComplete() }.flowOn(Dispatchers.IO)
+
+}
