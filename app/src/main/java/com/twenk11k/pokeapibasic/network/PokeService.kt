bb74fcd52644fb3d67048409f24a021760125e0b
twenk11k <selim_demiral@outlook.com>
2021-09-13 19:22:14 +0300

    Add fetchPokemonInfo method to PokeService

--- app/src/main/java/com/twenk11k/pokeapibasic/network/PokeService.kt
+++ app/src/main/java/com/twenk11k/pokeapibasic/network/PokeService.kt
@@ -1,12 +1,17 @@
 package com.twenk11k.pokeapibasic.network
 
+import com.twenk11k.pokeapibasic.model.PokemonInfo
 import com.twenk11k.pokeapibasic.model.PokemonResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface PokeService {
 
     @GET("pokemon/")
     suspend fun fetchPokemonResponse(): Response<PokemonResponse>
+
+    @GET("pokemon/{name}")
+    suspend fun fetchPokemonInfo(@Path("name") name: String): Response<PokemonInfo>
 
 }
