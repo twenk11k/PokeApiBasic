5e60c6b0d12972f7c3a7951de89d48929a1d8859
twenk11k <selim_demiral@outlook.com>
2021-09-13 19:23:20 +0300

    Create PokemonInfo model

---
+++ app/src/main/java/com/twenk11k/pokeapibasic/model/PokemonInfo.kt
@@ -0,0 +1,20 @@
+package com.twenk11k.pokeapibasic.model
+
+import com.google.gson.annotations.SerializedName
+
+data class PokemonInfo(
+    val name: String,
+    @SerializedName("types") val types: List<PokemonType>,
+    @Transient var imageUrl: String
+) {
+
+    data class PokemonType(
+        val slot: Int,
+        val type: Type
+    )
+
+    data class Type(
+        val name: String
+    )
+
+}
