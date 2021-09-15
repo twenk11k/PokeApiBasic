ac02f0f65c76bbaceb32b725188ab7448f653a6e
twenk11k <selim_demiral@outlook.com>
2021-09-15 22:53:04 +0300

    Remove SerializedName annotation from types parameter in PokemonInfo

--- app/src/main/java/com/twenk11k/pokeapibasic/data/model/PokemonInfo.kt
+++ app/src/main/java/com/twenk11k/pokeapibasic/data/model/PokemonInfo.kt
@@ -1,9 +1,7 @@
 package com.twenk11k.pokeapibasic.data.model
 
-import com.google.gson.annotations.SerializedName
-
 data class PokemonInfo(
     val name: String,
-    @SerializedName("types") val types: List<PokemonType>,
+    val types: List<PokemonType>,
     @Transient var imageUrl: String
 )
