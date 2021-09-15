4a69fc2a2b710ccacd81756a874cb29c03a165ec
twenk11k <selim_demiral@outlook.com>
2021-09-15 20:36:24 +0300

    Add toast binding adapter

--- app/src/main/java/com/twenk11k/pokeapibasic/binding/ViewBinding.kt
+++ app/src/main/java/com/twenk11k/pokeapibasic/binding/ViewBinding.kt
@@ -1,6 +1,7 @@
 package com.twenk11k.pokeapibasic.binding
 
 import android.view.View
+import android.widget.Toast
 import androidx.databinding.BindingAdapter
 
 object ViewBinding {
@@ -12,6 +13,14 @@ object ViewBinding {
             View.GONE
         } else {
             View.VISIBLE
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter("toast")
+    fun bindToast(view: View, text: String?) {
+        text?.let {
+            Toast.makeText(view.context, it, Toast.LENGTH_SHORT).show()
         }
     }
 
