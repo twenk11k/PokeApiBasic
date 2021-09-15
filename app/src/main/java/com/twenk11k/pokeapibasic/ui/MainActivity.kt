b5834b803264b5d7733bbc86ec5fe2a861c237d5
twenk11k <selim_demiral@outlook.com>
2021-09-15 20:33:42 +0300

    Add MainViewModel as variable via data binding in MainActivity

--- app/src/main/java/com/twenk11k/pokeapibasic/ui/MainActivity.kt
+++ app/src/main/java/com/twenk11k/pokeapibasic/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.twenk11k.pokeapibasic.ui
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.twenk11k.pokeapibasic.R
 import com.twenk11k.pokeapibasic.binding.DataBindingActivity
 import com.twenk11k.pokeapibasic.databinding.ActivityMainBinding
@@ -10,9 +11,15 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : DataBindingActivity() {
 
     private val binding: ActivityMainBinding by binding(R.layout.activity_main)
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setBindings()
+    }
+
+    private fun setBindings() {
+        binding.vm = mainViewModel
         binding.lifecycleOwner = this@MainActivity
     }
 
