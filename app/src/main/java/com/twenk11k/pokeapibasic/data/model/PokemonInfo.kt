a3eef4017ca0633c67e26ff660f788002c8b23da
twenk11k <selim_demiral@outlook.com>
2021-09-15 23:32:04 +0300

    Add height and width strings to PokemonInfo model

--- app/src/main/java/com/twenk11k/pokeapibasic/data/model/PokemonInfo.kt
+++ app/src/main/java/com/twenk11k/pokeapibasic/data/model/PokemonInfo.kt
@@ -3,5 +3,7 @@ package com.twenk11k.pokeapibasic.data.model
 data class PokemonInfo(
     val name: String,
     val types: List<PokemonType>,
+    val height: String,
+    val weight: String,
     @Transient var imageUrl: String
 )
